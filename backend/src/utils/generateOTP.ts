adbacef6f0215ff102adda3e96ec010cd4648a2e
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-07-11 14:27:54 +0200

    H

--- backend/src/utils/generateOTP.ts
+++
@@ -1,7 +0,0 @@
-export const generateOTP = (length = 6) => {
-  const chars = '0123456789'
-  let result = ''
-  for (let i = length; i > 0; --i)
-    result += chars[Math.floor(Math.random() * chars.length)]
-  return result
-}
